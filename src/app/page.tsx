a09ef13a870a21eb326d77463cbde281111ece57
andreadev237 <andreadev237@gmail.com>
2025-04-20 14:35:27 +0200

    Added Dev only modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li>CAMBIAMO QUALCOSA</li>
+          <li>SOLO DEV</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
